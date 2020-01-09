f241662418c3158b2db10b9352e0ee85cf6b13b1
ssunhomail@hanmail.net <3e4r5t6y&>
2020-01-10 01:18:45 +0900

    React Counter with useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 import './App.css'
 
 const App: React.FC = () => {
+  const [count, setCount] = React.useState(0)
+
+  const onIncrease = () => {
+    setCount(count + 1)
+  }
+
+  const onDecrease = () => {
+    setCount(count - 1)
+  }
+
   return (
-    <div>Hello World</div>
+    <div>
+      <p> {count} </p>
+      <button onClick={onIncrease}>increase</button>
+      <button onClick={onDecrease}>decrease</button>
+    </div>
   )
 }
 
